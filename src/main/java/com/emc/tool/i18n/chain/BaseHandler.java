f4f3478c56ca1559712f23462efe84e2bbbb777b
weichx <Xi.Weicheng@emc.com>
2014-09-12 13:12:31 +0800

    add base class for sub handler class

---
+++ src/main/java/com/emc/tool/i18n/chain/BaseHandler.java
@@ -0,0 +1,73 @@
+package com.emc.tool.i18n.chain;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+
+public abstract class BaseHandler {
+
+	protected String attrName;
+
+	public BaseHandler(String attrName) {
+		super();
+		this.attrName = attrName;
+	}
+
+	protected boolean handle(Request request, Response response) {
+
+		if (StringUtils.isEmpty(attrName)) {
+			System.out.println("Attribute name has not been setted!");
+			return false;
+		}
+
+		Document doc = request.getDocument();
+
+		List<?> selectNodes2 = doc.selectNodes("//@" + attrName);
+
+		// need total size.
+		System.out.println(String.format("Size:%s", selectNodes2.size()));
+
+		for (Object object : selectNodes2) {
+
+			Attribute attribute = (Attribute) object;
+			String pName = attribute.getParent().getName();
+			String attrVal = attribute.getValue();
+
+			if (StringUtils.isEmpty(attrVal) || (attrVal.trim().startsWith("{") && attrVal.trim().endsWith("}"))) {
+				continue;
+			}
+
+			String id = attrVal.trim().replace("*", "must").replaceAll("[\\W&&[^\\s]]", "").replaceAll("\\s+", ".")
+					.toLowerCase();
+
+			String key = StringUtils.join(
+					new String[] { request.getNamespace(), pName.toLowerCase(), attrName.toLowerCase(), id }, ".");
+
+			if (key.endsWith(".")) {
+				key = key + "n" + new Date().getTime();
+			}
+
+			if (response.getProps().containsKey(key)) {
+				System.err.println(String.format("Existing Key:%s", key));
+
+				if (!response.getProps().get(key).equals(attrVal)) {
+					key = key + ".n" + new Date().getTime();
+					response.getProps().put(key, attrVal);
+					// need property.
+					System.out.println(String.format("%s=%s", key, attrVal));
+				}
+			} else {
+				response.getProps().put(key, attrVal);
+				// need property.
+				System.out.println(String.format("%s=%s", key, attrVal));
+			}
+
+			attribute.setValue(StringUtils.replace("{$('key')}", "key", key));
+		}
+
+		return true;
+	}
+}
--- src/main/java/com/emc/tool/i18n/chain/TitleAttributeHandler.java
+++ src/main/java/com/emc/tool/i18n/chain/TitleAttributeHandler.java
@@ -1,71 +1,19 @@
 package com.emc.tool.i18n.chain;
 
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.lang.StringUtils;
-import org.dom4j.Attribute;
-import org.dom4j.Document;
-
-public class TitleAttributeHandler implements IAttributeHandler {
+public class TitleAttributeHandler extends BaseHandler implements IAttributeHandler {
 
 	public static final String ATTRIBUTE_NAME = "title";
+	
+	public TitleAttributeHandler() {
+		super(ATTRIBUTE_NAME);
+	}
 
-	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean invoke(Request request, Response response) {
 
 		System.out.println("TitleAttributeHandler.invoke()");
 
-		Document doc = request.getDocument();
-
-		List selectNodes2 = doc.selectNodes("//@" + ATTRIBUTE_NAME);
-
-		// need total size.
-		System.out.println(String.format("Size:%s", selectNodes2.size()));
-
-		for (Object object : selectNodes2) {
-
-			Attribute attribute = (Attribute) object;
-
-			String pName = attribute.getParent().getName();
-
-			String attrVal = attribute.getValue();
-
-			if (attrVal.startsWith("{") && attrVal.endsWith("}")) {
-				continue;
-			}
-
-			String id = attrVal.trim().replace("*", "must").replaceAll("[\\W&&[^\\s]]", "").replaceAll("\\s+", ".")
-					.toLowerCase();
-
-			String key = StringUtils
-					.join(new String[] { request.getNamespace(), pName.toLowerCase(), ATTRIBUTE_NAME.toLowerCase(), id },
-							".");
-
-			if (key.endsWith(".")) {
-				key = key + "n" + new Date().getTime();
-			}
-
-			if (response.getProps().containsKey(key)) {
-				System.err.println(String.format("Existing Key:%s", key));
-
-				if (!response.getProps().get(key).equals(attrVal)) {
-					key = key + ".n" + new Date().getTime();
-					response.getProps().put(key, attrVal);
-					// need property.
-					System.out.println(String.format("%s=%s", key, attrVal));
-				}
-			} else {
-				response.getProps().put(key, attrVal);
-				// need property.
-				System.out.println(String.format("%s=%s", key, attrVal));
-			}
-
-			attribute.setValue(StringUtils.replace("{$('key')}", "key", key));
-		}
-
-		return true;
+		return handle(request, response);
 	}
 
 }
